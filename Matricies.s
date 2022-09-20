7c129635024a2f4fe095df10ecc24a6ed00542a8
Reagan Armstrong <rarmst0@gmail.com>
2022-09-20 12:38:10 -0400

    Added Data

--- Matricies.s
+++ Matricies.s
@@ -6,6 +6,17 @@
 main:
 
 exit:
-mov r7, #0x1
-mov r0, #65
-swi 0x11
+    mov R1, #0
+    mov R0, #0x18
+    swi 0x123456
+
+.data
+
+.balign 4
+B: .skip 16
+.balign 4
+A: .skip 16
+.balign 4
+C: .word 0
+D: .skip 36
+
